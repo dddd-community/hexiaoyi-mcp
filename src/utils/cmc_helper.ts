eee5dec971a5b987b4452ba5306b440cb7e67983
dddd-lol <community@dddd.lol>
2025-05-16 00:09:19 +0800

    Update some get token price function

--- src/utils/cmc_helper.ts
+++ src/utils/cmc_helper.ts
@@ -1,41 +1,57 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 
 class CMCHelper {
-
-  private readonly apiKey: string;
+  private apiKey: string;
+  private client: AxiosInstance | null = null;
+  private baseURL: string = 'https://pro-api.coinmarketcap.com';
 
   constructor(apiKey: string) {
     this.apiKey = apiKey;
   }
 
-  private async fetchData(url: string): Promise<any> {
+  private initClient() {
+    if (!this.client) {
+      this.client = axios.create({
+        baseURL: this.baseURL,
+        headers: {
+          'X-CMC_PRO_API_KEY': this.apiKey,
+          Accept: 'application/json',
+        },
+      });
+    }
+  }
+
+  /**
+   * 获取指定合约地址的代币价格信息
+   * @param contractAddress 合约地址
+   * @param networkId 网络ID
+   * @returns 返回代币价格信息或null
+   */
+  public async getTokenPriceByContractAddress(
+    contractAddress: string,
+    networkId: number
+  ): Promise<number | null> {
+    this.initClient();
     try {
-      const response = await axios.get(url);
-      return response.data;
-    } catch (error) {
-      throw new Error(`Failed to fetch data: ${error}`);
+      const response = await this.client!.get('/v4/dex/pairs/quotes/latest', {
+        params: {
+          contract_address: contractAddress,
+          network_id: networkId,
+        },
+      });
+      const data = response.data;
+      if (data && data.data) {
+        return data.data;
+      } else {
+        console.warn('未找到代币价格信息');
+        return null;
+      }
+    } catch (error: any) {
+      console.error('获取代币价格信息时出错：', error.message);
+      return null;
     }
   }
 
-  async getPriceData(tokenSymbol: string): Promise<any> {
-    const baseUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
-    const params = {
-      symbol: tokenSymbol,
-      convert: 'USD',
-      cmc_pro_api_key: this.apiKey,
-    };
-    const url = `${baseUrl}?${new URLSearchParams(params).toString()}`;
-
-    const data = await this.fetchData(url);
-
-    const capitalizedSymbol = tokenSymbol.toUpperCase();
-    const tokenData = data.data[capitalizedSymbol]?.quote?.USD;
-
-    if (!tokenData) {
-      throw new Error(`Token data for symbol ${tokenSymbol} not found.`);
-    }
-    return tokenData;
-  }
 }
 
 export default CMCHelper;
